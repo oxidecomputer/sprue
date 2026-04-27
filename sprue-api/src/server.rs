e9183cf5b0008d3493c75e704897129aae5dcbbb
augustuswm <gusmayo@gmail.com>
2026-04-27 13:18:56 -0500

    Fix merge. Add idempotency test

--- sprue-api/src/server.rs
+++ sprue-api/src/server.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use dropshot::ApiDescription;
+use dropshot::{ApiDescription, ServerBuilder};
 use slog::Logger;
 use std::net::SocketAddr;
 

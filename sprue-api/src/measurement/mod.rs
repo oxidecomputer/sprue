a1408cb2384303836956d063950cba85ab2f373e
Augustus Mayo <gusmayo@gmail.com>
2026-07-29 13:04:35 -0500

    Update sprue-api/src/measurement/mod.rs
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- sprue-api/src/measurement/mod.rs
+++ sprue-api/src/measurement/mod.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use dice_verifier::{Corim, ReferenceMeasurements, ReferenceMeasurementsError};
 use futures::stream::{FuturesUnordered, StreamExt};
 use regex::bytes::Regex;

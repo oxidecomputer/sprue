390af18556ee8c6b05fe8270e4bdc269c9023d42
Augustus Mayo <augustus@oxidecomputer.com>
2026-08-14 10:26:51 -0500

    Bump version (#20)

--- sprue-sdk/src/generated/sdk.rs
+++ sprue-sdk/src/generated/sdk.rs
@@ -12114,7 +12114,7 @@ pub mod types {
 ///
 /// Shared Oxide VM support services
 ///
-/// Version: 0.2.0
+/// Version: 0.3.1
 pub struct Client {
     pub(crate) baseurl: String,
     pub(crate) client: reqwest::Client,
@@ -12155,7 +12155,7 @@ impl Client {
 
 impl ClientInfo<()> for Client {
     fn api_version() -> &'static str {
-        "0.2.0"
+        "0.3.1"
     }
 
     fn baseurl(&self) -> &str {
